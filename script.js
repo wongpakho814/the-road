7d85a075eb5d109a5104263c76bc7e1be1471c5a
wongpakho814 <wongpakho814@gmail.com>
2023-05-10 17:43:48 +1000

    Implemented navbar

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Navbar
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+let i = 0;
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.style.cssText = `background-color: ${colors[i++]}`;    
+});
+
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");  
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+// End of navbar
+
 // Popular tours cards
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
   item.onclick = () => {
